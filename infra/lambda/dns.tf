0c6d21b65fcac942a702073ef58be50f7a25c32a
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-04-11 12:30:40 +0200

    count fix

--- infra/lambda/dns.tf
+++ infra/lambda/dns.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "cert_validation" {
 
   name    = aws_acm_certificate.api_cert[0].domain_validation_options[count.index].resource_record_name
   type    = aws_acm_certificate.api_cert[0].domain_validation_options[count.index].resource_record_type
-  zone_id = data.aws_route53_zone.primary.zone_id
+  zone_id = data.aws_route53_zone.primary[0].zone_id
   records = [aws_acm_certificate.api_cert[0].domain_validation_options[count.index].resource_record_value]
   ttl     = 60
 }
