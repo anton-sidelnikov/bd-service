f97db81e50fa6faf341ea197b84ed876b1e99382
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-04-11 23:54:06 +0200

    deploy and policy fix

--- infra/lambda/iam.tf
+++ infra/lambda/iam.tf
@@ -18,9 +18,8 @@ resource "aws_iam_role" "lambda_exec" {
   }
 }
 
-resource "aws_iam_policy_attachment" "lambda_dynamo_policy" {
-  name       = "attach_dynamo_policy-${terraform.workspace}"
-  roles      = [aws_iam_role.lambda_exec.name]
+resource "aws_iam_role_policy_attachment" "lambda_dynamo_policy" {
+  role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
